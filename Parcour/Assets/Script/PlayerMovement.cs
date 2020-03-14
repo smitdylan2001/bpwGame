f141464835aee27e48e24715d1279776ba16a44d
Dylan Smit <dylan.smit@hotmail.com>
2020-03-14 18:33:38 +0100

    Added respawn Fixed many issues Added basic first level ideas

--- Parcour/Assets/Script/PlayerMovement.cs
+++ Parcour/Assets/Script/PlayerMovement.cs
@@ -7,69 +7,102 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody rigidbody;
-    bool isGrounded;
+    public bool isGrounded;
     float disstanceToTheGround;
-    int counter = 0;
     public bool isBig;
     float playerScale = 1;
     Vector3 temp;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
         disstanceToTheGround = GetComponentInChildren<Collider>().bounds.extents.y;
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, disstanceToTheGround + 0.1f);
+        //isGrounded = Physics.Raycast(transform.position, Vector3.down, disstanceToTheGround + 0.1f);
     }
 
     void Start()
     {
         isBig = true;
         transform.localScale = new Vector3(1f, 1f, 1f);
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        Cursor.lockState = CursorLockMode.Locked;
     }
+    
 
     void Update()
     {
         Movement();
         CheckMoonJump();
-        Resize();
-
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isBig = !isBig;
+        }
     }
 
     private void FixedUpdate()
     {
+        Resize();
+    }
+
+    void OnCollisionEnter()
+    {
+        isGrounded = true;
     }
 
     private void Movement()
     {
         float vert = Input.GetAxis("Vertical");
         float hor = Input.GetAxis("Horizontal");
+        
 
         Vector3 moveDir = vert * transform.forward + hor * transform.right;
         if (isGrounded == true)
         {
-            rigidbody.MovePosition(transform.position + moveDir.normalized * moveSpeed * Time.deltaTime * moveSpeed);
+            rigidbody.MovePosition(transform.position + moveDir.normalized * moveSpeed * Time.deltaTime * playerScale);
         }
         else
         {
             Vector3 temp = rigidbody.velocity;
-            rigidbody.MovePosition(transform.position + moveDir.normalized * moveSpeed * Time.deltaTime * moveSpeed);
+            rigidbody.MovePosition(transform.position + moveDir.normalized * moveSpeed * Time.deltaTime * playerScale);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rigidbody.AddForce(jump * jumpForce * playerScale, ForceMode.Force);
+            isGrounded = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = 20;
+        }
+        else 
+        {
+            moveSpeed = 10;
+        }
+
     }
 
     private void CheckMoonJump()
     {
         if (Input.GetKey(KeyCode.E))
         {
-            Physics.gravity = new Vector3(Physics.gravity.x, -1, Physics.gravity.z);
+            Physics.gravity = new Vector3(Physics.gravity.x, -3, Physics.gravity.z);
             if (Input.GetKeyDown(KeyCode.E))
             {
-                rigidbody.AddForce(0, 150, 0);
+                rigidbody.AddForce(0, 300, 0);
+                isGrounded = false;
+
             }
 
         }
         else
         {
-            Physics.gravity = new Vector3(Physics.gravity.x, -9.8f, Physics.gravity.z);
+            Physics.gravity = new Vector3(Physics.gravity.x, -13f, Physics.gravity.z)* playerScale;
         }
 
         if (Input.GetKeyUp(KeyCode.E))
@@ -80,19 +113,17 @@ public class PlayerMovement : MonoBehaviour
 
     private void Resize()
     {
-        
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            isBig = !isBig;
-        }
 
         if (isBig == true && playerScale < 1)
         {
-            playerScale += 0.01f;
+            playerScale += 0.05f;
+
+            
         }
         if (isBig == false && playerScale > 0.4)
         {
-                playerScale -= 0.01f;
+            playerScale -= 0.05f;
+
         }
         transform.localScale = new Vector3(playerScale, playerScale, playerScale);
 
---
+++ Parcour/Assets/Script/Respawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    
+    public GameObject player;
+    public Transform SpawnPoint;
+
+    void OnCollisionEnter()
+    {
+        player.transform.position = SpawnPoint.transform.position;
+    }
+}
